cdc1095097d06318557c818bae3da3a9f14074ea
ialvarezrod <agencia@tobe.mx>
2015-11-10 11:25:30 -0600

    Update footer.php
    jhgh

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
                 <div class="footer clearfix">
-                    <div class="pull-left">&copy; 2015. SATOB ADMIN Powered By <a href="http://themeforest.net/user/Kopyov">Tobe Coorporativo</a></div>
+                    <div class="pull-left">&copy; 2015. SATOB ADMIN Powered <a href="http://themeforest.net/user/Kopyov">Tobe Coorporativo</a></div>
                     <div class="pull-right icons-group">
                         <a href="#"><i class="icon-screen2"></i></a>
 	        	<a href="#"><i class="icon-balance"></i></a>
